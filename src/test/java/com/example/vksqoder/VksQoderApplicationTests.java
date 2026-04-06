629ff8f9ee1612496f5007a0b237652aa2cc0ff0
Vd <vostrlog@gmail.com>
2026-04-06 21:16:52 +0300

    Initial commit

---
+++ src/test/java/com/example/vksqoder/VksQoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vksqoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VksQoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
